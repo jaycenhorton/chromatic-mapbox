a5ff38c8abde6b5e628702fd348b1c056fae4108
jaycen horton <jaycen.horton@gmail.com>
2018-12-27 13:53:13 -0700

    add patch to fix chromatic

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,15 @@
 import { configure } from '@storybook/react';
 import 'storybook-chromatic';
 
+// Manually defining some functions to make chromatic work (fails if excluded)
+if (!window.URL.createObjectURL) {
+  Object.defineProperty(window.URL, 'createObjectURL', { value: () => {} });
+}
+// Manually defining some functions to make chromatic work (warns if the above is included whilst the following is excluded)
+if (!window.URL.revokeObjectURL) {
+  Object.defineProperty(window.URL, 'revokeObjectURL', { value: () => {} });
+}
+
 const req = require.context('../src/stories', true, /\.story\.js$/);
 configure(() => {
   req.keys().forEach(filename => req(filename));
